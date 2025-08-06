6660caf9ffd8ad8f0a857cd70e29957c91555167
Rheygine Medel <quic_rmedel@quicinc.com>
2025-08-06 15:39:56 -0700

    QtiExtension: Use vendor.mpctl.init.complete in SFExtension
    This change uses the vendor.mpctl.init.complete prop to determine
    perf hal's status
    
    Change-Id: I6e164a25f690a8145113bf8f21625f63ec82cd63
    CRs-Fixed: 4245150

--- services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.cpp
+++ services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.cpp
@@ -606,8 +606,8 @@ void QtiSurfaceFlingerExtension::qtiSetContentFps(uint32_t contentFps) {
             return;
         }
 
-        auto perf_hal_status = base::GetProperty("init.svc.perf2-hal-1-0", "");
-        if (perf_hal_status == "stopped") {
+        auto perf_hal_status = base::GetIntProperty("vendor.mpctl.init.complete", 0);
+        if (!perf_hal_status) {
             ALOGV("Perf-Hal service is stopped number of attempts %d", mQtiFailedAttempts);
             mQtiFailedAttempts += 1;
             if (mQtiFailedAttempts > (int) contentFps / 2) {
